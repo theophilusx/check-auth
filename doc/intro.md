48cb6d8129772883985252151928ae08d1e75262
Tim Cross <theophilusx@gmail.com>
2017-07-17 16:08:48 +1000

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to check-auth
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/check_auth/core_test.clj
@@ -0,0 +1,7 @@
+(ns check-auth.core-test
+  (:require [clojure.test :refer :all]
+            [check-auth.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
