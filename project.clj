9dd19630d0e8df9a04e09ec7db0ed42cdea49d73
Tim Cross <theophilusx@gmail.com>
2017-09-05 15:46:07 +1000

    Added logback logging support

--- project.clj
+++ project.clj
@@ -3,11 +3,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.3.5"]
+                 [org.clojure/tools.logging "0.4.0"]
+                 [ch.qos.logback/logback-classic "1.2.3"]
                  [jcifs/jcifs "1.3.18"]
                  [org.clojars.pntblnk/clj-ldap "0.0.12"]
                  [cprop "0.1.11"]
                  [clojure-lanterna "0.9.7"]
-                 [org.clojure/tools.cli "0.3.5"]
                  [clj-time "0.14.0"]]
   :main ^:skip-aot check-auth.core
   :target-path "target/%s"
